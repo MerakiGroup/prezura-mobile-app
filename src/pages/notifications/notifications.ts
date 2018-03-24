89eaccb3e9ff49962a4747a1b3fa4b015c681cd4
Charith Wickramasinghe <charithw@99x.lk>
2018-03-24 13:21:15 +0530

    alert service added

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 /**
  * Generated class for the NotificationsPage page.
@@ -10,16 +10,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-notifications',
-  templateUrl: 'notifications.html',
+  selector: "page-notifications",
+  templateUrl: "notifications.html"
 })
 export class NotificationsPage {
+  max = 100;
+  min = 0;
+  bloodPressure = 0;
+  bpColor = "";
+  diabLegs = 0;
+  dlColor: "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NotificationsPage');
+    console.log("ionViewDidLoad NotificationsPage");
+
+    this.bloodPressure = this.generateRandomValue();
+    this.bpColor = this.getColorCode(this.bloodPressure);
+    this.diabLegs = this.generateRandomValue();
+    this.dlColor = this.getColorCode(this.diabLegs);
+
+    console.log(this.bloodPressure);
   }
 
+  generateRandomValue = () => {
+    return Math.random() * (this.max - this.min) + this.min;
+  };
+
+  getColorCode = value => {
+    if (value > 0 && value < 25) {
+      return "#27ae60";
+    } else if (value >= 25 && value < 50) {
+      return "#e67e22";
+    } else if (value >= 50 && value < 75) {
+      return "#f39c12";
+    } else {
+      return "#c0392b";
+    }
+  };
 }
