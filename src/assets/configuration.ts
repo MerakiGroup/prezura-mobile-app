a0e4143bb477d56581fda3691dbad7c6b95e96c2
CharithWick <CharithW@99x.lk>
2018-03-31 21:27:51 +0530

    login with cognito added

---
+++ src/assets/configuration.ts
@@ -0,0 +1,15 @@
+export default {
+  AWS: {
+    credentials: {
+      accessKey: 'AKIAIX7WOUPM3CQ4T6IQ',
+      secretKey: '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'
+    },
+    S3: {
+      bucketName: ''
+    },
+    Cognito: {
+      userPoolId: 'us-east-1_k8dFEySw8',
+      clientId: '4el43rgpsbla9qkr9pkfbcrpf2'
+    }
+  }
+};
---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HeatmapComponent } from './heatmap/heatmap';
+@NgModule({
+	declarations: [HeatmapComponent],
+	imports: [],
+	exports: [HeatmapComponent]
+})
+export class ComponentsModule {}
